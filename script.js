bcd70d890b6b37f8d6c80c54b41569393c87a4b5
silydevco <syly8004@gmail.com>
2024-07-29 13:53:33 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-const whatsappNumber = "1234567890";
-
-function contactWhatsApp(service) {
-    const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(`שלום, אני מעוניין/ת ב${service}.`)}`;
-    window.open(url, '_blank');
-}
